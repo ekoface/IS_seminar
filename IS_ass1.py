12a8741c1de7346a83d38f5ee4716025469a98a8
marprac <125833530+marprac@users.noreply.github.com>
2024-11-26 17:10:04 +0100

    popravu in preveru fitness

--- IS_ass1.py
+++ IS_ass1.py
@@ -5,29 +5,36 @@ import json
 
 def fitness_func(solution):
     preference_score = np.sum(solution * P.flatten())
+    #print(preference_score)
 
     penalty = 0
     penalty += np.sum(solution * A.flatten())
+    #print(penalty)
 
     reshaped_sol = solution.reshape((num_reviewers, num_papers))
+    #print(reshaped_sol)
 
     #penalty for exceeding reviewer capacity, not meeting min reviews per paper, exceeding max reviews per paper
     per_reviewer = np.sum(reshaped_sol, axis=1)
     per_paper = np.sum(reshaped_sol, axis=0)
     penalty += np.sum(per_paper < min_reviews_per_paper)
+    #print(penalty)
     penalty += np.sum(per_paper > max_reviews_per_paper)
+    #print(penalty)
     penalty += np.sum(per_reviewer > reviewer_capacity)
+    #print(penalty)
 
     # how many papers have reviewers that are friends
     co_review_matrix = np.dot(reshaped_sol, reshaped_sol.T)
     friend_review_counts = F * co_review_matrix
-    penalty = np.sum(friend_review_counts) // 2
-    
+    penalty += np.sum(friend_review_counts) // 2
+    #print(penalty)
+
     #how many friends reviewed papers that their friends authored
     authored_papers_by_friends = np.dot(F, A)
     penalty_matrix = reshaped_sol * authored_papers_by_friends
     penalty += np.sum(penalty_matrix)
-
+    #print(penalty)
     return preference_score - penalty
 
 
@@ -44,7 +51,7 @@ def initial_population(num_reviewers, num_papers, population_size):
 #     return offspring
 
 
-with open('datasets/easy_dataset_1.json', 'r') as file:
+with open('datasets/hard_dataset_2.json', 'r') as file:
     data = json.load(file)
 
 num_generations = 100
@@ -56,16 +63,21 @@ num_reviewers = data['num_reviewers']
 reviewer_capacity = data['reviewer_capacity']
 min_reviews_per_paper = data['min_reviews_per_paper']
 max_reviews_per_paper = data['max_reviews_per_paper']
+#print(num_papers, num_reviewers, reviewer_capacity, min_reviews_per_paper, max_reviews_per_paper)
 
 P = np.array(data['preferences'])
+#print(P)
 F = np.array(data['friendships'])
+#print(F)
 A = np.array(data['authorship'])
+#print(A)
 
 initial_pop = initial_population(num_reviewers, num_papers, population_size)
-print(initial_pop)
+# print(initial_pop)
 
-rez = fitness_func(initial_pop[0])
-print(rez)
+for i in range(population_size):
+    rez = fitness_func(initial_pop[i])
+    print(rez)
 
 # ga_instance = pygad.GA(num_generations=num_generations,
 #                        num_parents_mating=num_parents_mating,
